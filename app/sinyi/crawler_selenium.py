e9ca6b8d35bc831721216472dbcf47e8bbfed11c
Eason Lin <uu9924079@gmail.com>
2025-05-21 20:58:53 +0800

    feat: sinyi crawler

--- app/sinyi/crawler_selenium.py
+++ app/sinyi/crawler_selenium.py
@@ -7,28 +7,36 @@ from dotenv import load_dotenv
 from libs.utils import get_page_content, write_file, use_selenium, render_images, render_link
 load_dotenv()
 
-def write_normal(driver, soup, start_url):
-  data = []
-  page = 1
-  items = soup.select('#buy_container .ddhouse')
-  for item in items:
-    image_lst = []
-    img_count = int(item.select_one('div.img_count_txt').text)
-    first_image_src = item.select_one('.item_img img').get('src')
-    for i in range(0, img_count):
-      if (chr(65 + i) == 'E' or chr(65 + i) == 'F'):
-        img_count += 1
-        continue
-      current_image_src = first_image_src.replace('A', chr(65 + i))
-      image_lst.append(current_image_src)
-    title = item.select_one('.item_title').text
-    link = item.select_one('a')['href']
-    areaAndFloor = item.select_one('.detail_line2')
-    area = areaAndFloor.select('span')[2].text.replace('\n', '')
-    floor = areaAndFloor.select('span')[4].text.replace('\n', '')
-    address = item.select('.detail_line2')[1].text.replace('\n', '')
-    price = item.select_one('.detail_price').text.replace('\n', '')
-    data.append([title, price, address, floor, area, image_lst, link])
+def write_normal(driver, start_url):
+  def _helper(start_page, data):
+    page = start_page
+    while True:
+        soup = get_page_content(driver, start_url.replace('/1.html', f'/{page}.html'))
+        items = soup.select('#buy_container .ddhouse')
+        if not items:
+            break
+        for item in items:
+            image_lst = []
+            img_count = int(item.select_one('div.img_count_txt').text)
+            first_image_src = item.select_one('.item_img img').get('src')
+            for i in range(0, img_count):
+                if chr(65 + i) in ['E', 'F']:
+                    img_count += 1
+                    continue
+                current_image_src = first_image_src.replace('A', chr(65 + i))
+                image_lst.append(current_image_src)
+            title = item.select_one('.item_title').text
+            link = item.select_one('a')['href']
+            areaAndFloor = item.select_one('.detail_line2')
+            area = areaAndFloor.select('span')[2].text.replace('\n', '')
+            floor = areaAndFloor.select('span')[4].text.replace('\n', '')
+            address = item.select('.detail_line2')[1].text.replace('\n', '')
+            price = item.select_one('.detail_price').text.replace('\n', '')
+            data.append([title, price, address, floor, area, image_lst, link])
+        page += 1
+    return data
+        
+  data = _helper(1, [])
   columns = ['title', 'price', 'address', 'floor', 'area', 'images', 'link']
   df = pd.DataFrame(data, columns=columns)
   json_output = df.to_json(orient='records')
@@ -47,7 +55,7 @@ def main():
         print('If .env is not exist, please create one at the root directory')
         print('Example: SINYI_FILTER_URL=https://www.sinyi.com.tw/rent/list/Taipei-city/103-zip/index.html')
         sys.exit()
-    soup = get_page_content(driver, start_url)
-    write_normal(driver, soup, start_url)
+    start_url = start_url.replace('/index.html', '/1.html')
+    write_normal(driver, start_url)
 
 main()
